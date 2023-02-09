9d454b2f38489a120bb41af5fc90e3024ea22439
Dani Ponce Mendoza <danniscreamer@gmail.com>
2023-02-09 13:51:07 -0600

    se borraron los comentarios anteriores

--- segundaclase/src/routes/About.jsx
+++ segundaclase/src/routes/About.jsx
@@ -2,52 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios'
 
-// function About(){
-//     const [posts, setPost] = useState([]);
-
-//     useEffect(()=> {
-//         axios.get('https://emojihub.yurace.pro/api/random')
-//         .then((response) => setPost(response.data))
-//     }, [])
-
-// return(
-//  <>
-//  <nav>
-//             <ul>
-//                 <li>
-//                     <Link to={'/'}>Home</Link>
-//                 </li>
-//                 <li>
-//                     <Link to={'/about'}>About</Link>
-//                 </li>
-//                 <li>
-//                     <Link to={'/contact'}>Contact</Link>
-//                 </li>
-//             </ul>
-//         </nav>
-//  <h1>Hola esto es About</h1>
-//  {posts.map((post) =>{
-//     return(
-//         <div key={post.id}>
-//             <h1>{post.name}</h1>
-//         </div>
-
-//     )
-//  })}
-//  </>
-// )
-// }
 
 
  function About(){
-//   const [datos, setDatos] = useState([]);
-//   useEffect(() => {
-//     axios.get(URL).then((response) =>{
-//         setDatos(response.data)
-//     })
-//   },[]);
 
-//  if(!datos) return null
 
 const [info,setinfo] = useState([])
 const getData = async() => {
